106db96f3e3978d266aaef7e2770fa94f750c552
JustinJira <jus.jira@gmail.com>
2024-03-20 08:27:42 +0700

    feat: data loading

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import type { PageData } from './$types';
+	import { generations } from './generations';
+
+	export let data: PageData;
+</script>
+
+{#each data.monsters as monster (monster.id)}
+	<p>{monster.id}: {monster.name}</p>
+{/each}
