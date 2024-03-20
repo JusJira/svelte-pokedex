eef92dac10d9a75c036d99a52de1a78376056298
JustinJira <jus.jira@gmail.com>
2024-03-20 08:44:12 +0700

    style: added pokemon styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,6 +5,24 @@
 	export let data: PageData;
 </script>
 
-{#each data.monsters as monster (monster.id)}
-	<p>{monster.id}: {monster.name}</p>
-{/each}
+<div class="flex flex-row flex-wrap justify-center">
+    {#each generations as generation (generation.id)}
+    <a class="m-[10px] px-[5px] py-[10px] bg-[#f9f9f9] text-[#333] border-[1px] border-black hover:bg-[#eee]" href="/">{generation.main_region}</a>
+    {/each}
+</div>
+
+<div class="flex flex-row flex-wrap justify-center">
+	{#each data.monsters as monster (monster.id)}
+		<div
+			class="w-[100px] m-[10px] p-[10px] flex flex-col items-center justify-center relative bg-[#eee] hover:bg-[#ddd]"
+		>
+			<div>
+				<img src={monster.image} alt={monster.name} />
+				{monster.name}
+			</div>
+			<div class="absolute top-2 left-2 text-[#aaa]">
+				{monster.id}
+			</div>
+		</div>
+	{/each}
+</div>
