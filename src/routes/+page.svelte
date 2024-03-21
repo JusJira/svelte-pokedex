f4aa0c375eab46836619838da4ba9cd8b0dec793
JustinJira <jus.jira@gmail.com>
2024-03-21 13:20:20 +0700

    feat: monster search

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,28 +1,85 @@
 <script lang="ts">
 	import type { PageData } from './$types';
 	import { generations } from './generations';
+	import type { IndexMonster } from './+page';
+	import { page } from '$app/stores';
+	import { goto } from '$app/navigation';
+	import Monster from './Monster.svelte';
 
 	export let data: PageData;
+
+	let searchParams = '';
+
+	let form = {
+		searchString: ''
+	};
+
+	$: selectedMonsters = data.monsters.filter((monster) => {
+		return monster.name.toLowerCase().includes(searchParams.toLowerCase());
+	});
+
+	let monsterId: string = $page.url.searchParams.get('monsterId') || '';
+
+	$: monsterId = $page.url.searchParams.get('monsterId') || '';
+	$: monster = data.monsters.find((monster) => monster.id === monsterId);
+	$: monsterId2 = $page.url.searchParams.get('monsterId2') || '';
+	$: monster2 = data.monsters.find((monster) => monster.id === monsterId2);
+
+	$: seletctedGenerationId = $page.url.searchParams.get('generation-id') || '';
+
+	const updateSearchParams = (key: string, value: string) => {
+		const searchParams = new URLSearchParams($page.url.searchParams);
+		searchParams.set(key, value);
+		goto(`?${searchParams.toString()}`);
+	};
+
+	const submitSearch = (e: Event) => {
+		searchParams = form.searchString;
+	};
 </script>
 
 <div class="flex flex-row flex-wrap justify-center">
-    {#each generations as generation (generation.id)}
-    <a class="m-[10px] px-[5px] py-[10px] bg-[#f9f9f9] text-[#333] border-[1px] border-black hover:bg-[#eee]" href="/">{generation.main_region}</a>
-    {/each}
-</div>
-
-<div class="flex flex-row flex-wrap justify-center">
-	{#each data.monsters as monster (monster.id)}
-		<div
-			class="w-[100px] m-[10px] p-[10px] flex flex-col items-center justify-center relative bg-[#eee] hover:bg-[#ddd]"
+	<button
+		class="generation-button"
+		class:active={seletctedGenerationId === 'all'}
+		on:click={() => updateSearchParams('generation-id', 'all')}
+		>All
+	</button>
+	{#each generations as generation (generation.id)}
+		<button
+			class="generation-button"
+			class:active={seletctedGenerationId === generation.id.toString()}
+			on:click={() => updateSearchParams('generation-id', generation.id.toString())}
 		>
-			<div>
-				<img src={monster.image} alt={monster.name} />
-				{monster.name}
-			</div>
-			<div class="absolute top-2 left-2 text-[#aaa]">
-				{monster.id}
-			</div>
-		</div>
+			{generation.main_region}
+		</button>
 	{/each}
 </div>
+
+<form on:submit|preventDefault={submitSearch} class="flex items-center justify-center my-4 flex-row gap-1">
+	<input
+		class="rounded-[5px] w-[200px] px-2 py-1 border-2 border-black"
+		type="text"
+		bind:value={form.searchString}
+		placeholder="Pokemon Name"
+	/>
+	<input
+		class="h-full rounded-[5px] px-2 py-1 border-2 border-black"
+		type="submit"
+		value="Search"
+	/>
+</form>
+<div class="flex flex-row flex-wrap justify-center">
+	{#each selectedMonsters as monster (monster.id)}
+		<Monster {monster} {updateSearchParams} isInteractive />
+	{/each}
+</div>
+
+<style lang="postcss">
+	.active {
+		@apply bg-neutral-800 text-white !important;
+	}
+	.generation-button {
+		@apply m-[10px] px-[10px] py-[5px] bg-[#f9f9f9] text-[#333] border-[1px] border-black hover:bg-[#eee];
+	}
+</style>
---
+++ src/routes/Monster.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import type { IndexMonster } from './+page';
+
+	export let monster: IndexMonster;
+	export let updateSearchParams: (key: string, value: string) => void;
+	export let isInteractive: boolean = false;
+</script>
+
+<!-- svelte-ignore a11y-click-events-have-key-events -->
+<div
+	class="w-[100px] m-[10px] p-[10px] flex flex-col items-center justify-center relative bg-[#eee] hover:bg-[#ddd]"
+>
+	<!-- svelte-ignore a11y-no-static-element-interactions -->
+	<div on:click={() => (isInteractive ? updateSearchParams('monsterId', monster.id) : () => {})}>
+		<div>
+			<img src={monster.image} alt={monster.name} />
+			{monster.name}
+		</div>
+		<div class="absolute top-2 left-2 text-[#aaa]">
+			{monster.id}
+		</div>
+	</div>
+    {#if isInteractive}
+		<!-- svelte-ignore a11y-no-static-element-interactions -->
+		<div on:click={() => updateSearchParams('monsterId2', monster.id)}>Add Monster 2</div>
+	{/if}
+</div>
